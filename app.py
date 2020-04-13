dfe069b5a10eca206efbd5ae8e05e1bcbd38a83c
benoit_arnlt <benar77@gmail.com>
2020-04-13 14:51:31 +0200

    changed json generation and added new routes to generate multiple weapons via the API

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import json
 from main import weapon_generation
 app = Flask(__name__, static_url_path='/static/')
 
-def create_json_resp(w: Weapon):
-    response = app.response_class(
-        response = w.generate_json(),
+def json_type_response(value):
+        return app.response_class(
+        response = value,
         status = 200,
         mimetype='application/json'
     )
-    return response
+
 
 @app.route('/')
 def index():
@@ -21,16 +21,46 @@ def index():
 def assault_rifle():
     w = Weapon()
     w.generate("ASSAULT_RIFLE")
-    return create_json_resp(w)
+    return json_type_response(json.dumps(w.__dict__))
 
 @app.route('/shotgun')
 def shotgun():
     w = Weapon()
     w.generate("SHOTGUN")
-    return create_json_resp(w)
+    return json_type_response(json.dumps(w.__dict__))
 
 @app.route('/sniper_rifle')
 def sniper():
     w = Weapon()
     w.generate("SNIPER_RIFLE")
-    return create_json_resp(w)
+    return json_type_response(json.dumps(w.__dict__))
+
+@app.route('/assault_rifle/<number>')
+def assault_rifle_multiple(number):
+    number = int(number)
+    weapon_list = []
+    for i in range(number):
+        w = Weapon()
+        weapon_list.append(w)
+        weapon_list[i].generate("ASSAULT_RIFLE")
+    return json_type_response(json.dumps([w.__dict__ for weapon_list in weapon_list]))
+
+@app.route('/shotgun/<number>')
+def assault_rifle_multiple(number):
+    number = int(number)
+    weapon_list = []
+    for i in range(number):
+        w = Weapon()
+        weapon_list.append(w)
+        weapon_list[i].generate("SHOTGUN")
+    return json_type_response(json.dumps([w.__dict__ for weapon_list in weapon_list]))
+
+@app.route('/sniper_rifle/<number>')
+def assault_rifle_multiple(number):
+    number = int(number)
+    weapon_list = []
+    for i in range(number):
+        w = Weapon()
+        weapon_list.append(w)
+        weapon_list[i].generate("SNIPER_RIFLE")
+    return json_type_response(json.dumps([w.__dict__ for weapon_list in weapon_list]))
